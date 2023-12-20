ae29e4fd5e2511df3a2897c97ea6d4934452a023
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2023-12-20 19:49:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let str1=str.split("");
+	let obj = {};
+	str1.forEach((a)=>{
+	    if(obj[a]){
+	        obj[a]++;
+	    }
+	    else{
+	        obj[a]=1;
+	    }
+	});
+	// console.log(Object.values(obj));
+	for (let i of Object.keys(obj)){
+	    if (obj[i]==1){
+	        return i;
+	    }
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
